93afbaa8faca4cb62c650a76290739169248192d
Austin Ellis <Austin@Janets-iMac.local>
2021-01-15 20:53:32 -0500

    Added webmanifest file and started service-worker.js & db.js

---
+++ public/service-worker.js
@@ -0,0 +1,11 @@
+const FILES_TO_CACHE = [
+    "/",
+    "db.js",
+    "index.html",
+    "index.js",
+    "style.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
+];
+
+const CACHE_NAME = "static-cache-v2";
