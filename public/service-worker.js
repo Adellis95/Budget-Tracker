dedfeab0265db51fa1b2637f0b634fae5f9dae06
Austin Ellis <Austin@Janets-iMac.local>
2021-01-16 17:23:38 -0500

    Updated service-worker.js with install eventlistener

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,3 +10,12 @@ const FILES_TO_CACHE = [
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
+
+// install service-worker
+self.addEventListener("install", function(evt) {
+    evt.waitUntil(caches.open(CACHE_NAME).then(cache => {
+        console.log("Files successfully pre-cached");
+        return cache.addAll(FILES_TO_CACHE);
+    }));
+    self.skipWaiting();
+});
